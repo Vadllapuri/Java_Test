89d97f992d49070937381a444b77bd71f33f711e
Raji <rajeshwarivadllapuri@gmail.com>
2020-07-14 10:07:47 +0530

    Test

---
+++ src/javaPrograms/July.java
@@ -0,0 +1,13 @@
+package javaPrograms;
+
+public class July {
+	
+
+	public static void main(String[] args) {
+		July myobj = new July();
+		int a = 10;
+		//System.out.println(myobj.a);
+		System.out.println(a);
+	}
+
+}
